26f8fe7e7d4afcb0cd3f82b1d9d963752a47ea06
Aditya Singh <s.addy1317@gmail.com>
2024-09-15 00:02:19 +0530

    feat: Camouflage Training with Genetic Algorithms Part 2
    - Breed
    - BreedNewPopulataion

--- Assets/Scripts/PopulationManager.cs
+++ Assets/Scripts/PopulationManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class PopulationManager : MonoBehaviour
 {
@@ -44,5 +45,55 @@ public class PopulationManager : MonoBehaviour
             BreedNewPopulation();
             elasped = 0;
         }
+    }
+
+    GameObject Breed(GameObject parent1, GameObject parent2)
+    {
+        Vector3 pos = new Vector3(Random.Range(-9, 9), Random.Range(-4.5f, 4.5f), 0);
+        GameObject offspring = Instantiate(personPrefab, pos, Quaternion.identity);
+        DNA dna1 = parent1.GetComponent<DNA>();
+        DNA dna2 = parent2.GetComponent<DNA>();
+
+        //swap parent data
+
+        if (Random.Range(0, 1000) > 5)
+        {
+            offspring.GetComponent<DNA>().r = Random.Range(0, 10) < 5 ? dna1.r : dna2.r;
+            offspring.GetComponent<DNA>().g = Random.Range(0, 10) < 5 ? dna1.r : dna2.g;
+            offspring.GetComponent<DNA>().b = Random.Range(0, 10) < 5 ? dna1.r : dna2.b;
+        }
+        else
+        {
+            offspring.GetComponent<DNA>().r = Random.Range(0.0f, 1.0f);
+            offspring.GetComponent<DNA>().g = Random.Range(0.0f, 1.0f);
+            offspring.GetComponent<DNA>().b = Random.Range(0.0f, 1.0f);
+        }
+        return offspring;
+    }
+     
+    private void BreedNewPopulation()
+    {
+        List<GameObject> newPopulation = new List<GameObject>();
+
+        //get rid of unfit individuals
+        //List<GameObject> sortedList = population.OrderBy(o => o.GetComponent<DNA>().timeToDie).ToList(); // normal order
+        List<GameObject> sortedList = population.OrderByDescending(o => o.GetComponent<DNA>().timeToDie).ToList(); // decreased order
+
+        population.Clear();
+
+        //breed upper half of sorted list
+        for (int i = (int)(sortedList.Count / 2.0f) - 1; i < sortedList.Count - 1; i++)
+        {
+            population.Add(Breed(sortedList[i], sortedList[i+1]));
+            population.Add(Breed(sortedList[i +1], sortedList[i]));
+        }
+
+        for(int  i = 0; i < population.Count; i++)
+        {
+            Destroy(sortedList[i]);
+        }
+
+        generation++;
+
     }
 }
